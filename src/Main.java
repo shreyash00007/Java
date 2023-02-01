e361feae549d465f0c62052601fadd7526dd215d
shreyash dhanawade <68527969+shreyash00007@users.noreply.github.com>
2023-02-01 13:38:09 +0530

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello GitHub with Java");
+        System.out.println("Hello GitHub with Java is best");
     }
 }
